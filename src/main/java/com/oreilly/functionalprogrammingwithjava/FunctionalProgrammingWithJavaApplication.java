763334a695331e775aa0e53f813dac6f1174309e
ikane <kaneelhadji2610@yahoo.fr>
2019-07-21 17:31:53 +0200

    first commit

---
+++ src/main/java/com/oreilly/functionalprogrammingwithjava/FunctionalProgrammingWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.oreilly.functionalprogrammingwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalProgrammingWithJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalProgrammingWithJavaApplication.class, args);
+    }
+
+}
